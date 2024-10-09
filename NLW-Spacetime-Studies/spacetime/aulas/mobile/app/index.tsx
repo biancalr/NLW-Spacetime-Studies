166130c2cec593415a7e4df7247cceca199ecf4d
bianc <bianc@host.docker.internal>
2024-10-08 21:13:27 -0300

    Adicionando módulos

---
+++ NLW-Spacetime-Studies/spacetime/aulas/mobile/app/index.tsx
@@ -0,0 +1,85 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import * as SecureStore from 'expo-secure-store'
+import { useRouter } from 'expo-router'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { useEffect } from 'react'
+
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/47769a06b13d6ce1abb1',
+}
+
+export default function App() {
+  const router = useRouter()
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '47769a06b13d6ce1abb1',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    // console.log(
+    //   'response',
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGithubOAuthCode(code)
+    }
+  }, [response])
+
+  return (
+    <View className=" flex-1 items-center px-8 py-10">
+      <View className="flex-1 items-center justify-center space-y-6">
+        <NLWLogo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua Cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar Lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com ❤️ no NLW da Rocketseat
+      </Text>
+    </View>
+  )
+}
+// SHOWMETHECODE
